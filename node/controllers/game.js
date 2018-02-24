6e2a75e8748816e699e4476eb90845746497f0b6
sm398 <sm398@pc3-053-l.cs.st-andrews.ac.uk>
2018-02-24 21:49:54 +0000

    Implemented return players object with name

--- node/controllers/game.js
+++ node/controllers/game.js
@@ -20,12 +20,12 @@ module.exports =
                 uc.addUser(user).then(function (userId) {
                         getIncompleteGame(gameName).then(function (games) {
                             if (games.length === 0) {
-                                createGame(gameName, userId).then(function (res) {
+                                createGame(gameName, userId, user.name).then(function (res) {
                                     fulfill(userId);
                                 });
                             }
                             else {
-                                linkGameToUser(games[0], userId).then(function (res) {
+                                linkGameToUser(games[0], userId, user.name).then(function (res) {
                                     uc.linkUserToGame(userId, games[0]._id).then(function (res) {
                                         fulfill(userId);
                                     });
@@ -47,22 +47,13 @@ module.exports =
          */
         getGame: function (userId) {
             return new Promise(function (fulfill, reject) {
-                db.filter({remainingPlayers: 0}, collectionName).then(function (res, req) {
-                    let found = false;
-                    for (let i = 0; i < res.length; i++) {
-                        let game = res[i];
-                        for (let j = 0; j < game.players.length; j++) {
-                            let playerId = game.players[j];
-                            if (playerId == userId) {
-                                fulfill(game);
-                                found = true;
-                            }
-                        }
-                    }
-                    if (!found) {
+                db.filter({remainingPlayers: 0, players: {$elemMatch: {userId: ObjectID(userId)}}}, collectionName).then(function (res, req) {
+                    if (res.length === 0) {
                         console.log("Not found or game incomplete");
                         reject({error: "Game not ready"});
                     }
+                    else
+                        fulfill();
                 })
             })
         },
@@ -123,12 +114,12 @@ let getIncompleteGame = function (game) {
  * @param {string} userId - the ID of the user the lobby is created for in the first place
  * @returns {Promise<Object>} - returns the mongodb result of adding the game to the database
  */
-let createGame = function (game, userId) {
+let createGame = function (game, userId, name) {
     let newGame =
         {
             name: game,
             remainingPlayers: (getGameSize(game) - 1),
-            players: [userId]
+            players: [{userId: userId, name: name}]
         };
     console.log(newGame.players);
     return db.add(newGame, collectionName);
@@ -140,9 +131,8 @@ let createGame = function (game, userId) {
  * @param {string} userId - the ID of the user the lobby is created for in the first place
  * @returns {Promise<Object>} - returns the mongodb result of adding the game to the database
  */
-let linkGameToUser = function (game, userId) {
-
-    return db.update({_id: game._id}, {$push: {players: userId}, $inc: {remainingPlayers: -1}}, collectionName);
+let linkGameToUser = function (game, userId, name) {
+    return db.update({_id: game._id}, {$push: {players: {userId: userId, name: name}}, $inc: {remainingPlayers: -1}}, collectionName);
 };
 
 /**
